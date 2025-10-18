f27d7dbc3d4d3050f17ccc351c62ddbf43f0eb15
julienogabriel <julienogabriel4@gmail.com>
2025-10-18 16:27:47 +0300

    deuxieme commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,23 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
+const dev = process.argv.includes('dev');
+const base = dev ? '' : '/Myportfolio'; // ⚠️ Remplace NOM_DU_REPO par le nom de ton dépôt GitHub
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html',
+      precompress: false
+    }),
+    paths: {
+      base
+    }
+  },
+  preprocess: vitePreprocess()
 };
 
 export default config;
